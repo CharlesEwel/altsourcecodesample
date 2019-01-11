664832031a36bf144d9a8a2e712eafa898e814a2
Charles Ewel <charles.ewel@dealers1.com>
2019-01-11 13:31:53 -0800

    initial commit

---
+++ AltSourceCodeSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AltSourceCodeSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
